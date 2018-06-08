4c011c9813d8049c6113eccf20454747f2670b5b
Jesse Carter <35956744+JJ-Carter@users.noreply.github.com>
2018-06-08 08:53:22 -0400

    Update auto-unrar.php

--- auto-unrar.php
+++ auto-unrar.php
@@ -1,9 +1,12 @@
 <?php
 
+// Path to the .rar files
+$rarPath = '/home/files/';
+
 // Path to place the unrar file
 $unrarPath = '/home/done/';
 
-$files = glob('/home/files/*.{r00,txt,pdf,mkv,mp3}', GLOB_BRACE);
+$files = glob($rarPath . '*.{r00,txt,pdf,mkv,mp3}', GLOB_BRACE);
   foreach ($files as $file) 
   {
     if (stripos($file, '.r00') !== false) 
