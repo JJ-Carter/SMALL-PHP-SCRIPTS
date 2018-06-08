15c3a08518ea7af5b63e387d01d3434571c39cd4
Jesse Carter <35956744+JJ-Carter@users.noreply.github.com>
2018-06-08 08:48:14 -0400

    Update auto-unrar.php

--- auto-unrar.php
+++ auto-unrar.php
@@ -1,1 +1,18 @@
+<?php
 
+// Path to place the unrar file
+$unrarPath = '/home/done/';
+
+$files = glob('/home/files/*.{r00,txt,pdf,mkv,mp3}', GLOB_BRACE);
+  foreach ($files as $file) 
+  {
+    if (stripos($file, '.r00') !== false) 
+    {
+      $string="unrar e '$file' $unrarPath";
+      exec($string);
+    } else {
+      echo $file . " is not in a rar format\n";
+    }
+  }
+
+?>
